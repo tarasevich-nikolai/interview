e656f844c97399e386909891ab4cd5046f12320e
Mikalai U Tarasevich <mikalai.u.tarasevich@godeltech.com>
2017-10-09 17:15:09 +0300

    GDC

---
+++ src/main/java/tarasevich/nikolai/interview/ds/array/Gdc.java
@@ -0,0 +1,15 @@
+package tarasevich.nikolai.interview.ds.array;
+
+/**
+ * @author Nikolai Tarasevich
+ */
+public class Gdc {
+
+    public int gdc1(int a, int b) {
+        if (b == 0) {
+    }
+
+    public static void main(String args[]) {
+        int a =8, b =4;
+    }
+}
