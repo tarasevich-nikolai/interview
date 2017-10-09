f10578bd67c12e794fe07f328ee4e632fb71c54e
tarasevich-nikolai <dormidontn1@gmail.com>
2017-10-09 20:24:37 +0300

    Gdc

--- src/main/java/tarasevich/nikolai/interview/ds/array/Gdc.java
+++ src/main/java/tarasevich/nikolai/interview/ds/array/Gdc.java
@@ -7,9 +7,14 @@ public class Gdc {
 
     public int gdc1(int a, int b) {
         if (b == 0) {
+            return a;
+        }
+        return gdc1(b, a % b);
     }
 
     public static void main(String args[]) {
-        int a =8, b =4;
+        int a =8, b = 5;
+        Gdc gdc = new Gdc();
+        System.out.println(gdc.gdc1(a, b));
     }
 }
