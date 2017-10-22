0122ddf952c5ed8205b9e0e9957b49a697ebc419
tarasevich-nikolai <dormidontn1@gmail.com>
2017-10-22 12:57:43 +0300

    Binary search

---
+++ src/main/java/tarasevich/nikolai/interview/algo/search/binary/BinarySearch.java
@@ -0,0 +1,25 @@
+package tarasevich.nikolai.interview.algo.search.binary;
+
+import tarasevich.nikolai.interview.algo.search.util.SearchHelper;
+
+/**
+ * @author nikolai.tarasevich
+ */
+public class BinarySearch {
+
+    public static int search(int[] arr, int lookingFor) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == lookingFor) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = SearchHelper.generateIntArray();
+        int lookingFor = arr[9];
+        int result = search(arr, lookingFor);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/tarasevich/nikolai/interview/algo/search/util/SearchHelper.java
@@ -0,0 +1,28 @@
+package tarasevich.nikolai.interview.algo.search.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author nikolai.tarasevich
+ */
+public class SearchHelper {
+
+    public static int[] generateIntArray() {
+        return generateIntArray(10);
+    }
+
+    public static int[] generateIntArray(int size) {
+        List<Integer> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+            list.add(i);
+        Collections.shuffle(list);
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
