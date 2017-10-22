9ce3ed7bf6b5ff54e7a135c334d489aa3df512c8
tarasevich-nikolai <dormidontn1@gmail.com>
2017-10-22 23:07:20 +0300

    Sublist search

--- src/main/java/tarasevich/nikolai/interview/algo/search/sublist/SublistSearch.java
+++ src/main/java/tarasevich/nikolai/interview/algo/search/sublist/SublistSearch.java
@@ -2,16 +2,33 @@ package tarasevich.nikolai.interview.algo.search.sublist;
 
 import tarasevich.nikolai.interview.algo.search.util.SearchHelper;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SublistSearch {
 
-    public static int search(int[] arr, int lookingFor) {
+    public static int search(List<Integer> list, List<Integer> sublist) {
+
+        x: for (int i = 0; i < list.size(); i++) {
+
+            int temp = i;
+            for (Integer lookingFor: sublist) {
+                if (lookingFor != list.get(temp) || temp > list.size()) {
+                    continue x;
+                }
+                temp++;
+            }
+
+            return i;
+        }
+
         return -1;
     }
     
-    public static void main(String[] args) {
-        int[] arr = SearchHelper.generateSortedIntArray();
-        int lookingFor = arr[7];
-        int result = search(arr, lookingFor);
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+        LinkedList<Integer> list = (LinkedList<Integer>) SearchHelper.generateIntList(LinkedList.class);
+        List<Integer> sublist = list.subList(6, 7);
+        int result = search(list, sublist);
         System.out.println(result);
     }
 }
--- src/main/java/tarasevich/nikolai/interview/algo/search/util/SearchHelper.java
+++ src/main/java/tarasevich/nikolai/interview/algo/search/util/SearchHelper.java
@@ -2,23 +2,44 @@ package tarasevich.nikolai.interview.algo.search.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * @author nikolai.tarasevich
  */
 public class SearchHelper {
 
+    public static List<Integer> generateIntList() {
+        return generateIntList(10);
+    }
+
+    public static List<Integer> generateIntList(Class listClass) throws InstantiationException, IllegalAccessException {
+        return generateIntList(10, listClass);
+    }
+
+    public static List<Integer> generateIntList(int size) {
+        List<Integer> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+            list.add(i);
+        Collections.shuffle(list);
+        return list;
+    }
+
+    public static List<Integer> generateIntList(int size, Class<List> listClass) throws IllegalAccessException, InstantiationException {
+        List<Integer> list = listClass.newInstance();
+        for (int i = 0; i < size; i++)
+            list.add(i);
+        Collections.shuffle(list);
+        return list;
+    }
+
     public static int[] generateIntArray() {
         return generateIntArray(10);
     }
 
     public static int[] generateIntArray(int size) {
-        List<Integer> list = new ArrayList<>(size);
-        for (int i = 0; i < size; i++)
-            list.add(i);
-        Collections.shuffle(list);
+        List<Integer> list = generateIntList(size);
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i] = list.get(i);
