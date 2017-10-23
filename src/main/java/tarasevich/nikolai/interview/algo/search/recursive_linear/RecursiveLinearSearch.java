5a26b3b656db36ff1129fa7a9a9dd0ab2e94e4fc
tarasevich-nikolai <dormidontn1@gmail.com>
2017-10-23 22:40:04 +0300

    Linear recursive search

--- src/main/java/tarasevich/nikolai/interview/algo/search/recursive_linear/RecursiveLinearSearch.java
+++ src/main/java/tarasevich/nikolai/interview/algo/search/recursive_linear/RecursiveLinearSearch.java
@@ -5,12 +5,22 @@ import tarasevich.nikolai.interview.algo.search.util.SearchHelper;
 public class RecursiveLinearSearch {
 
     public static int search(int[] arr, int lookingFor) {
-        return -1;
+        return linearSearch(arr, lookingFor, 0);
     }
-    
+
+    private static int linearSearch(int[] arr, int lookingFor, int index) {
+        if (arr[index] == lookingFor) {
+            return index;
+        }
+        if (index == arr.length) {
+            return -1;
+        }
+        return linearSearch(arr, lookingFor, index + 1);
+    }
+
     public static void main(String[] args) {
         int[] arr = SearchHelper.generateSortedIntArray();
-        int lookingFor = arr[7];
+        int lookingFor = arr[3];
         int result = search(arr, lookingFor);
         System.out.println(result);
     }
