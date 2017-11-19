9108e683b0812b7b8a9b39975a9c68f30b393899
tarasevich-nikolai <dormidontn1@gmail.com>
2017-11-19 22:05:47 +0300

    Data structure

---
+++ src/main/java/tarasevich/nikolai/interview/algo/sorting/DataStructure.java
@@ -0,0 +1,92 @@
+package tarasevich.nikolai.interview.algo.sorting;
+
+import java.util.function.Function;
+
+public class DataStructure {
+    
+    // Create an array
+    private final static int SIZE = 15;
+    private int[] arrayOfInts = new int[SIZE];
+    
+    public DataStructure() {
+        // fill the array with ascending integer values
+        for (int i = 0; i < SIZE; i++) {
+            arrayOfInts[i] = i;
+        }
+    }
+    
+    public void printEven() {
+        
+        // Print out values of even indices of the array
+        DataStructureIterator iterator = this.new EvenIterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+
+    public void print(DataStructureIterator iterator) {
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+
+    public void print(Function<Integer, Boolean> func) {
+        for (int x : arrayOfInts) {
+            if (func.apply(x)) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    interface DataStructureIterator extends java.util.Iterator<Integer> { } 
+
+    // Inner class implements the DataStructureIterator interface,
+    // which extends the Iterator<Integer> interface
+    
+    private class EvenIterator implements DataStructureIterator {
+        
+        // Start stepping through the array from the beginning
+        private int nextIndex = 0;
+        
+        public boolean hasNext() {
+            
+            // Check if the current element is the last in the array
+            return (nextIndex <= SIZE - 1);
+        }        
+        
+        public Integer next() {
+            
+            // Record a value of an even index of the array
+            Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
+            
+            // Get the next even element
+            nextIndex += 2;
+            return retValue;
+        }
+    }
+
+    public int[] getArrayOfInts() {
+        return arrayOfInts;
+    }
+
+    public static Boolean isEvenIndex(Integer i) {
+        return i % 2 == 0;
+    }
+
+    public static Boolean isOddIndex(Integer i) {
+        return i % 2 == 1;
+    }
+
+    public static void main(String s[]) {
+
+        // Fill the array with integer values and print out only
+        // values of even indices
+        DataStructure ds = new DataStructure();
+
+        ds.print(DataStructure::isEvenIndex);
+        ds.print(DataStructure::isOddIndex);
+    }
+}
--- src/main/java/tarasevich/nikolai/interview/algo/sorting/insertion/InsertionSorting.java
+++ src/main/java/tarasevich/nikolai/interview/algo/sorting/insertion/InsertionSorting.java
@@ -13,13 +13,13 @@ public class InsertionSorting {
     public static void main(String[] args) {
         int[] arr = SortingHelper.generateIntArray();
         System.out.println("Before " + Arrays.toString(arr));
-        int[] result = SelectionSort.sort(arr);
+        int[] result = InsertionSorting.sort(arr);
         System.out.println("After " + Arrays.toString(result));
     }
 
     public static int[] sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i -1; j > 0; j--) {
+            for (int j = i ; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
                     swap(arr, j - 1, j);
                 }
